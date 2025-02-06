5fbdde621b2dc7ff0fcc575e09eced17461daf3f
NotSoClevr <157626810+NotSoClevr@users.noreply.github.com>
2025-02-06 15:09:55 -0500

    add functionality

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const container = document.querySelector("#container");
 
 /*
-psuedocode
-
 create a 16x16 grid of divs
-
 */
 for (i = 1; i < 257; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("grid");
+    newDiv.setAttribute("id", `square${i}`)
     container.appendChild(newDiv);
 };
+
+// random background color generator
+function bGColor(frm, to) {
+  return Math.floor(Math.random() * (to - frm)) + frm;
+}
+
+let COLORS = [];
+
+while (COLORS.length < 100) {
+    COLORS.push(`rgb(${bGColor(0, 255)}, ${bGColor(0, 255)}, ${bGColor(0, 255)})`)
+}
+
+
+// change background color on hover
+
+const grid = document.querySelectorAll(".grid");
+
+grid.forEach((square, index) => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = COLORS[index % COLORS.length];
+    });
+})
